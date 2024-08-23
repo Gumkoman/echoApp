9a0a468019b0f0de0717290ae9f6a7d6d95acb7e
Gumkoman <gumkoman@gmail.com>
2024-08-23 22:52:49 +0200

    Removed debug msges

--- main.py
+++ main.py
@@ -392,7 +392,7 @@ class CustomTkinterApp(ttkb.Window):
                     time.sleep(1)
                     self.status_bar.config(text="Oczekiwanie na polaczenie")
                     text_button = self.get_main_mcptt_button(driver)
-                    print(f"text_button is {text_button}")
+                    # print(f"text_button is {text_button}")
                     if 'czenie' in text_button:
                         state = "start_recording"
                 elif state == "start_recording":
